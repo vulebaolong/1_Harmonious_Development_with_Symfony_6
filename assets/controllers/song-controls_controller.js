54b688df9caa70f983740e51dc60c80bfdb0aedf
Vu Le Bao Long <vulebaolong@gmail.com>
2023-10-11 09:13:43 +0700

    oke

---
+++ assets/controllers/song-controls_controller.js
@@ -0,0 +1,31 @@
+import {Controller} from '@hotwired/stimulus';
+import axios from "axios";
+
+/*
+ * This is an example Stimulus controller!
+ *
+ * Any element with a data-controller="hello" attribute will cause
+ * this controller to be executed. The name "hello" comes from the filename:
+ * hello_controller.js -> "hello"
+ *
+ * Delete this file or adapt it for your use!
+ */
+export default class extends Controller {
+    static values = {
+        infoUrl: String,
+    }
+    connect() {
+        console.log("song-controls_controller connected");
+    }
+
+    play(event) {
+        event.preventDefault();
+        // console.log(this.infoUrlValue);
+        axios.get(this.infoUrlValue)
+            .then((result)=>{
+                console.log(result)
+                const audio = new Audio(result.data.url);
+                audio.play();
+            })
+    }
+}
